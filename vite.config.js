34e199be604e5b163b199228a24d8001f7c1fc83
Vladimir Denisov <92729921+Vilodya@users.noreply.github.com>
2026-08-10 17:13:52 +0500

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/faq-accordeon-new/'
+  base: '/frontend-mentor-faq-accordeon/'
 })
